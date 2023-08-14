d6270631870720459edb9f7f9d29bc5eb327ca5a
Kamal-Goyal-2005 <127817467+Kamal-Goyal-2005@users.noreply.github.com>
2023-08-14 16:15:33 +0530

    Add files via upload

--- MYSQL.sql
+++ MYSQL.sql
@@ -1,50 +1,32 @@
-create table xxdemostudent(sid int,sname varchar(20),gender char, dob date);
-
-desc xxdemostudent;
-
-drop table xxdemostudent;
-
 show databases;
 
-use saas_finance_dev;
-
+use db2;
 
 Create table xxclient (
-id          		int,
-first_name  		varchar(240),
-last_name			varchar(240),
-user_name			varchar(240),
-email 				varchar(50),
-pwd					varchar(30),
+id          		int primary key auto_increment,
+first_name  		varchar(50),
+last_name			varchar(50),
+user_name			varchar(50),
+email 				varchar(50) not null,
+pwd					varchar(30) not null,
 Address_line1		varchar(240),
 Address_line2		varchar(240),
 Address_line3		varchar(240),
 contact_number		varchar(20),
-city				varchar(240),
-state				varchar(240),
-country				varchar(240),
-created_by			int,
-creation_date		date,
-last_update_by		int,
-last_update_date	date
+city				varchar(120),
+state				varchar(120),
+country				varchar(120)
 );
+
 desc xxclient;
 
-drop table xxclient;
-
-drop table xxmeeting;
-
 create table xxmeeting (
-Meeting_id			int,
-Title				varchar(100),
-Date				date,
+Meeting_id			int primary key auto_increment,
+Title				varchar(100) not null,
+Date				date not null,
 Duration			int,
 Meeting_password	varchar(30),
-Location			varchar(30),
-created_by			int,
-creation_date		date,
-last_update_by		int,
-last_update_date	date
+Location			varchar(30)
 );
 
 desc xxmeeting;
